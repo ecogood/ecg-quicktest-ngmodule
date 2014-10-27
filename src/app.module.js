f6201ccdd6eeea51ef104ccde3e6fc15717cf5dd
Nikolay Georgiev <nikolay@georgiev.io>
2014-10-27 15:01:25 +0100

    clean(app): remove unneeded comment

--- src/app.module.js
+++ src/app.module.js
@@ -1,15 +1,6 @@
 (function() {
   'use strict';
 
-  /**
-   * Requires:
-   * 1) parent modules:
-   * - ui.router
-   *
-   * 2) translations:
-   * set $scope.t = ecgQuicktestTexts[langShort];
-   *
-   */
   angular.module('ecg-quicktest', [
     'bootstrap'
   ]);
